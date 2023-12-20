1dbfd670d840941b3aef068426e647c2e6b36cdd
aizhan-bgz <baigazakova1999@gmail.com>
2023-12-20 15:06:21 +0600

    first commit

---
+++ src/taskCars/Main.java
@@ -0,0 +1,22 @@
+package taskCars;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HashMap<Car, Integer> cars = new HashMap<>();
+        cars.put(new Car("Toyota Camry"), 2017);
+        cars.put(new Car("Lexus LX 600"), 2021);
+        cars.put(new Car("Hyundai Sonata"), 2020);
+
+        for (Map.Entry<Car, Integer> i: cars.entrySet()) {
+            System.out.println(i.getKey() + ", " + i.getValue());
+        }
+
+        System.out.println("\nKeys: " + cars.keySet());
+        System.out.println("Values: " + cars.values());
+
+    }
+}
---
+++ src/taskContacts/Main.java
@@ -0,0 +1,24 @@
+package taskContacts;
+
+import taskCars.Car;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, Integer> contacts = new HashMap<>();
+
+        contacts.put("Shirin", 556744570);
+        contacts.put("Aizhan", 702090637);
+        contacts.put("Amir", 502097636);
+        contacts.put("Ayana", 779153625);
+        contacts.put("Alina", 552622553);
+
+        for (Map.Entry<String , Integer> i: contacts.entrySet()) {
+            System.out.println(i.getKey() + ", phone number: " + i.getValue());
+        }
+        System.out.println("\nNames: " + contacts.keySet());
+        System.out.println("Phone number: " + contacts.values());
+    }
+}
